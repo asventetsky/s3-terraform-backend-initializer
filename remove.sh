205eee609d8a465cd35dc37eaa8d79258fc2a160
asventetsky <artyom.sven@gmail.com>
2023-02-02 20:57:22 +0300

    Handle resources removal (#3)
    * implement remove resources workflow
    
    * add remove workflow
    
    * trigger new workflow
    
    * update removal
    
    * log request
    
    * log request
    
    * log request
    
    * add region
    
    * save list-object-versions output to file
    
    * fix command
    
    * fix command
    
    * specify region for bucket deletion

---
+++ remove.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+main() {
+  # parse flags
+  while getopts "r:b:t:a:s:" flag; do
+    case "${flag}" in
+      r) REGION=${OPTARG};;
+      b) S3_BUCKET_NAME=${OPTARG};;
+      t) DYNAMO_DB_TABLE_NAME=${OPTARG};;
+      a) ACCESS_KEY_ID=${OPTARG};;
+      s) SECRET_ACCESS_KEY=${OPTARG};;
+      \?) echo "Unknown flag, available flags are -r, -s and -d"; exit;;
+    esac
+  done
+
+  print_flag_values
+  set_aws_keys
+  remove_s3_bucket
+  if [[ -n $DYNAMO_DB_TABLE_NAME ]]; then
+    remove_dynamo_db_table
+  fi
+}
+
+print_flag_values() {
+  echo "Region: $REGION"
+  echo "S3 Bucket Name: $S3_BUCKET_NAME"
+  echo "DynamoDB table name: $DYNAMO_DB_TABLE_NAME"
+}
+
+set_aws_keys() {
+  if [[ -n $ACCESS_KEY_ID && -n $SECRET_ACCESS_KEY ]]; then
+    export AWS_ACCESS_KEY_ID=$ACCESS_KEY_ID
+    export AWS_SECRET_ACCESS_KEY=$SECRET_ACCESS_KEY
+    echo "Using AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from inputs"
+  else
+    echo "Using AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY from secrets"
+  fi
+}
+
+remove_s3_bucket() {
+  echo "Start listing object versions."
+  aws s3api list-object-versions \
+    --bucket "$S3_BUCKET_NAME" \
+    --region "$REGION" \
+    --output=json \
+    --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}' \
+    >> delete.json \
+    || exit
+  echo "Finish listing object versions."
+
+  echo "Start deleting objects."
+  aws s3api delete-objects \
+    --bucket "$S3_BUCKET_NAME" \
+    --region "$REGION" \
+    --delete file://delete.json \
+    || exit
+  echo "Finish deleting objects."
+
+  echo "Start removing bucket."
+  aws s3 rb "s3://$S3_BUCKET_NAME" --region "$REGION"
+  echo "Finish removing bucket."
+}
+
+remove_dynamo_db_table() {
+  aws dynamodb delete-table \
+    --table-name "$DYNAMO_DB_TABLE_NAME" \
+    --region "$REGION"
+}
+
+main "$@"; exit
